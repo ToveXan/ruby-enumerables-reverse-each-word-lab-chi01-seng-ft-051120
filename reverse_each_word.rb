c02169dc7aa324d00141dff950ace9bfc9f1951d
Zoe Xanos <zxanos@gmail.com>
2020-05-11 02:04:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,18 +1,7 @@
+
 def reverse_each_word(sentence)
-  array_sen = sentence.split(" ")
-  starting_here = []
-  
-  array_sen.each do |word|
-    word.reverse!
-    starting_here << word.split(" ")
-  end
-  starting_here.flatten
-  final = starting_here.join(" ")
-  final
-
-  new_sen.map { |x| x }
-  new_sen.collect { |x| x.reverse }
-  new_sen
+  new_array = sentence.split(" ")
+  new_array.map { |x| x }
+  new_array.collect { |x| x.reverse }
+  new_array
 end
-
-
